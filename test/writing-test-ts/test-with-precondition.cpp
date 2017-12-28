2235b69b7115538bef02e66e52fc1ad4f60f0f5a
Raffi Enficiaud <raffi.enficiaud@free.fr>
2017-12-28 10:46:45 +0100

    Unit test reproducing the issue

---
+++ test/writing-test-ts/test-with-precondition.cpp
@@ -0,0 +1,23 @@
+//  (C) Copyright Raffi Enficiaud 2017.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/test for the library home page.
+//
+//! @file
+//! checking the preconditions
+// *****************************************************************************
+
+#define BOOST_TEST_MODULE test_preconditions
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(fail,
+                     *boost::unit_test::precondition([](boost::unit_test::test_unit_id){ return false; }))
+{
+
+}
+BOOST_AUTO_TEST_CASE(success)
+{
+  BOOST_TEST(true);
+}
